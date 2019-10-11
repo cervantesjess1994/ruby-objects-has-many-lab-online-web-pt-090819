1c353341a0a1f1ba596e73505e3c7ef4717aaeb1
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-10-11 02:31:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,23 +7,20 @@ class Artist
 
   def initialize(name)
     @name = name
-    @songs = []
   end
 
   def add_song(song)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+    Song.all.select {|song| song.artist == self}
   end
 
   def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+    new_song = Song.new(name)
+    add_song(new_song)
+  end
+  
   end
 
   def self.song_count
-    @@song_count.size
+    Song.all.count
   end
 end
